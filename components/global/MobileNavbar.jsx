31427ed7481830f023d6aa6020729d249e50fbc9
Muhammad Muneeb <muhammadmuneeb12gh@gmail.com>
2025-06-24 13:47:23 +0500

    mobile navbar logo adjustment

--- components/global/MobileNavbar.jsx
+++ components/global/MobileNavbar.jsx
@@ -40,14 +40,21 @@ export default function MobileNavbar() {
         } p-5`}
         style={{ zIndex: 101 }}
       >
-        <li className="list-none font-bold text-lg">
+        <li className="list-none font-normal text-lg">
           <Link href="/">
+          <span className="font-black text-[17px] flex items-center">
             <img
-              className="mr-3"
-              src="/static/logos/logo_full.svg"
-              width="160"
+              className="mr-2 h-[35px] w-[35px] transform hover:rotate-360 hover:scale-75 transition-transform duration-500"
+              src="/static/logos/logo_no_text.png"
+              
             />
-          </Link>
+            
+                <span className="hover:text-fun-pink hover:-mt-2 transition-all duration-500 hover:duration-100 click:goodbyeLetterAnim">
+                  Muneeb.M
+                </span>
+    
+          </span>
+        </Link>
         </li>
         <button
           className="burger visible md:hidden"
